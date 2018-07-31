1cc6b1175873c4e0e0afcdb65f4a346ee5d41159
Brett Beekley <brett@science37.com>
2018-07-30 17:21:00 -0700

    Use delay between data requests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // Parameters
 const params = {
-  center: {lat: 34.0211667, lng: -118.4346565},
+  center: {lat: 34.0261897, lng: -118.4115412},
   zoom: 14,
   steps: 2,
   stepSize: 0.005,
+  delay: 500,
   destinations: [
     {
       address: '12121 Bluff Creek Dr #100, Los Angeles, CA 90094',
-      travelMode: 'TRANSIT',
+      travelMode: 'BICYCLING',
     },
   ],
 };
@@ -31,20 +32,39 @@ async function initMap() {
  */
 const createHeatmap = async map => {
   const coords = createCoords(params.steps, params.stepSize, params.center);
-  const data = await Promise.all(coords.map(async coord => {
+  const coordCount = coords.length;
+  const data = new google.maps.MVCArray([]);
+  refreshHeatmap(map, data);
+  console.log(`Starting calculation for ${coordCount} locations.`);
+  for (let i = 0; i < coordCount; i += 1) {
+    const coord = coords[i];
     const destination = params.destinations[0];
     try {
+      await delay(params.delay);
       coord.weight = await getTransitData(coord.location, destination.address, destination.travelMode);
     }
     catch (error) {
       console.log(error);
     }
-    return coord;
-  }));
-  new google.maps.visualization.HeatmapLayer({
-    data, map, radius: 100,
-  });
+    data.push(coord);
+    console.log(`${i+1}/${coordCount}`);
+  }
+  console.log('Done.')
+  console.log({data});
 };
+
+/**
+ * @description Refreshes heatmap with new data. NOTE: This is the only interface with `params`
+ * @param {Object} map - Google Maps API map instance
+ * @param {Object} data - Google Maps API map instance
+ */
+const refreshHeatmap = (map, data) => new google.maps.visualization.HeatmapLayer({
+  data,
+  map,
+  radius: 100,
+  opacity: 0.4,
+  maxIntensity: 3000,
+});
 
 /**
  * @description Create coord array
@@ -57,8 +77,8 @@ const createCoords = (steps, stepSize, center) => {
   // Create array of coordinates
   const coords = [];
   // Iterate from -steps to steps
-  for (let x = -steps; x < steps; x += 1) {
-    for (let y = -steps; y < steps; y += 1) {
+  for (let x = -steps; x <= steps; x += 1) {
+    for (let y = -steps; y <= steps; y += 1) {
       const coord = {
         location: new google.maps.LatLng(
           center.lat + x * stepSize,
@@ -81,17 +101,17 @@ const createCoords = (steps, stepSize, center) => {
  * @return {Object[]}
  */
 const getTransitData = async (origin, destination, travelMode, retryCount = 0) => new Promise((resolve, reject) => {
-  const retryThreshold = 5;
-  const retryWait = 1000;
   const request = {
     origin,
     destination,
     travelMode,
   };
   const directionsService = new google.maps.DirectionsService;
-  setTimeout(() => directionsService.route(request, (response, status) => {
+  directionsService.route(request, (response, status) => {
     if (status !== 'OK') return reject({ response, status });
     const duration = response.routes[0].legs[0].duration.value;
     resolve(duration);
-  }), retryWait);
+  });
 });
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
