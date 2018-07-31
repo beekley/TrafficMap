bd1a9b81bed3786b5ce22bb1e81323a3a0c53c91
Brett Beekley <brett@science37.com>
2018-07-31 16:39:03 -0700

    Log output formatted for http://contourmapcreator.urgr8.ch/

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const params = {
   center: {lat: 34.0261897, lng: -118.4115412},
   zoom: 14,
-  steps: 4,
+  steps: 3,
   stepSize: 0.0035,
   delay: 1000,
   destinations: [
@@ -10,13 +10,15 @@ const params = {
       address: '12121 Bluff Creek Dr #100, Los Angeles, CA 90094',
       travelMode: 'BICYCLING',
     },
-    // {
-    //   address: '5555 Valley Blvd, Los Angeles, CA 90032',
-    //   travelMode: 'DRIVING',
-    // },
+    {
+      address: '5555 Valley Blvd, Los Angeles, CA 90032',
+      travelMode: 'DRIVING',
+    },
   ],
 };
 let map = null;
+let stop = false;
+let data;
 
 /**
  * @description Google Maps API initialization function
@@ -38,31 +40,35 @@ const createHeatmap = async map => {
   const coords = createCoords(params.steps, params.stepSize, params.center);
   const coordCount = coords.length;
   const destCount = params.destinations.length;
-  const data = new google.maps.MVCArray([]);
+  data = new google.maps.MVCArray([]);
   console.log(`Starting calculation for ${coordCount} locations and ${destCount} destinations.`);
   for (let i = 0; i < coordCount; i += 1) {
+    if (stop) break;
     const coord = coords[i];
-    let sum = 0;
+    coord.durations = [];
     for (let j = 0; j < destCount; j += 1) {
       const destination = params.destinations[j];
       try {
         await delay(params.delay);
-        sum += await getTransitData(coord.location, destination.address, destination.travelMode);
+        coord.durations[j] = await getTransitData(coord.location, destination.address, destination.travelMode);
       }
       catch (error) {
         console.log(error);
       }
       console.log(`${i+1}/${coordCount} - destination ${j+1}`);
     }
-    coord.weight = sum;
+    coord.weight = coord.durations.reduce((sum, cur) => sum + cur, 0);
     data.push(coord);
   }
   console.log('Done.');
   console.log({ originaldata: data });
+  console.log('Generating output.');
+  console.log(createContourOutput(data.getArray()));
   console.log('Normalizing.');
   normalizeData(data);
   console.log({ normalizedData: data });
   createHeatmapLayer(map, data);
+
 };
 
 /**
@@ -71,7 +77,8 @@ const createHeatmap = async map => {
  */
 const normalizeData = data => {
   const min = Math.min(...data.getArray().map(datum => datum.weight));
-  // const max = Math.max(data.map(datum => datum.weight));
+  const max = Math.max(...data.getArray().map(datum => datum.weight));
+  console.log({ min, max });
   data.forEach((datum, i) => {
     datum.weight -= min;
     data.setAt(i, datum);
@@ -89,7 +96,7 @@ const createHeatmapLayer = (map, data) => new google.maps.visualization.HeatmapL
   map,
   radius: 70,
   opacity: 0.6,
-  maxIntensity: 2000,
+  maxIntensity: 4000,
   // dissipating: true,
 });
 
@@ -141,5 +148,43 @@ const getTransitData = async (origin, destination, travelMode, retryCount = 0) =
     resolve(duration);
   });
 });
+
+/**
+ * @description
+ * @param {Object} data
+ */
+const createContourOutput = data => {
+  // Get SE and NW corners
+  let minLat = Infinity;
+  let maxLat = -Infinity;
+  let minLong = Infinity;
+  let maxLong = -Infinity;
+  // Get dimensions (assumes square)
+  const dim = Math.sqrt(data.length);
+  // Create grid
+  const grid = [];
+  for (let i = 0; i < dim; i += 1) {
+    const row = [];
+    for (let j = 0; j < dim; j += 1) {
+      row.push(null);
+    }
+    grid.push(row);
+  }
+
+  data.forEach((datum, i) => {
+    const latlng = datum.location;
+    if (latlng.lat() > maxLat) maxLat = latlng.lat();
+    if (latlng.lat() < minLat) minLat = latlng.lat();
+    if (latlng.lng() > maxLong) maxLong = latlng.lng();
+    if (latlng.lng() < minLong) minLong = latlng.lng();
+    const row = dim - Math.floor(i / dim) - 1;
+    const col = i % dim;
+    grid[row][col] = datum.weight / 60;
+  });
+
+  // Create output string
+  const output = grid.map(row => row.join('	')).join('\n');
+  console.log({ NW: { maxLat, maxLong }, SE: { minLat, minLong }, output });
+};
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
